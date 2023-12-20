fdfed17269eed81264b7c8818b893c7808a5261f
Zamanbekm <z.kuanyshev03@mail.ru>
2023-12-20 20:09:38 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Register from "./Pages/registration/Register";
+import Login from "./Pages/Login";
+import Chat from "./Pages/chat/Chat";
+import Profile from "./Pages/Profile";
+import Services from "./Pages/services/Services";
+import AuthProvider from "./components/context/auth";
+import PrivateRoutes from "./PrivateRoutes";
+import React, {useState} from "react";
+import Admin from "./Pages/admin/admin";
+import ContactUs from "./Pages/ContactUs";
+import {LogOut} from "./components/Logout";
+import {ThemeContext} from "./components/context/ThemeContext";
+
+function App() {
+
+    const [night,setNight]=useState(JSON.parse(localStorage.getItem('nightMode'))||false);
+
+    window.addEventListener("beforeunload",  (ev) => {
+        ev.preventDefault();
+
+        return ()=> {LogOut()};
+
+    });
+    return (<div className={`${night?'nightMode':''}`}>
+        <ThemeContext.Provider value={{night, setNight}}>
+                <AuthProvider>
+
+                    <BrowserRouter>
+                        <Routes>
+                            <Route exaxt path='/' element={<Home/>} />
+                            <Route  path='/register' element={<Register/>} />
+                            <Route  path='*' element={<Navigate to={'/'}/>} />
+                            <Route  path='/login' element={<Login/>} />
+                            <Route  path='/services' element={<Services/>} />
+                            <Route  path='/contact' element={<ContactUs/>} />
+                            <Route  path={'/profile'}   element={<Profile/>}/>
+
+                            <Route element={<PrivateRoutes/>}>
+                                <Route path={'/admin'} element={<Admin/>}/>
+                                <Route  path='/chat' element={<Chat/>}  />
+
+                            </Route>
+
+
+                        </Routes>
+                    </BrowserRouter>
+
+                </AuthProvider>
+</ThemeContext.Provider>
+
+    </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/Pages/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>For any inquiries, please contact us at the following number:</p>
+            <p><strong>Phone:</strong> +7 (700) 679-2926</p>
+        </div>
+    );
+};
+
+export default ContactUs;
